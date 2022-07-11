02718a1d2ed927cff0f8485acd47cbc92ef97ddc
Alex <alexander.richardson@gmail.com>
2022-07-11 13:38:28 +0100

    offset parameter index by 1

--- parameter_index.py
+++ parameter_index.py
@@ -5,7 +5,7 @@ import sys
 	Takes job array index as input, prints parameters
 """
 
-index = int(sys.argv[1])
+index = int(sys.argv[1])-1
 FIRE_RATE_INTS = (np.arange(10).astype(int)+1)*2
 DECAY_FACTOR_INTS= (np.arange(10).astype(int)+1)*2
 N_CHANNELS = np.arange(5,10)
