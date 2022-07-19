836b1cd9c883445b93c2b3aeaa977048aa01e6ad
Alex <alexander.richardson@gmail.com>
2022-07-19 15:12:23 +0100

    running several types of morphing demos on eddie

--- emoji_morph.py
+++ emoji_morph.py
@@ -9,21 +9,31 @@ import sys
 
 N_CHANNELS=16
 N_BATCHES=4
-suffix=str(sys.argv[1])
+index=int(sys.argv[1])
 
 
-def train_emoji_sequence(filename_sequence,model_filename):
-	data = load_emoji_sequence(filename_sequence,1)
+def train_emoji_sequence(filename_sequence,model_filename,downsample):
+	data = load_emoji_sequence(filename_sequence,downsample)
 	ca = NCA(N_CHANNELS)
 	trainer = NCA_Trainer(ca,data,N_BATCHES,model_filename=model_filename)
 	trainer.train_sequence(4000,120)
 
 
 def main():
-	train_emoji_sequence(["mushroom_1f344.png",
-						  "lizard_1f98e.png",
-						  "rooster_1f413.png"],
-						  "emoji_sequence_mushroom_lizard_highres_"+suffix)
-
+	if index==1:
+		train_emoji_sequence(["mushroom_1f344.png",
+							  "lizard_1f98e.png",
+							  "rooster_1f413.png"],
+							  "emoji_sequence_mushroom_lizard_rooster_highres")
+	if index==2:
+		train_emoji_sequence(["skull_1f480.png",
+							  "rainbow_1f308.png",
+							  "rainbow_1f308.png"],
+							  "emoji_sequence_skull_rainbow_stable_highres")
+	if index==3:
+		train_emoji_sequence(["skull_1f480.png",
+							  "rainbow_1f308.png",
+							  "skull_1f480.png"],
+							  "emoji_sequence_skull_rainbow_skull_highres")
 
 main()
