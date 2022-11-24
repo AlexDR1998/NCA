c1a38eeecc07bcfb54c416bdc99d49d05af7ff23
Alex <alexander.richardson@gmail.com>
2022-11-24 17:02:38 +0000

    silly typo

--- training_explore.array.sh
+++ training_explore.array.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-#$ -N train_explore
+#$ -N training_explore
 #$ -cwd
 #$ -l h_rt=8:00:00
 
 #$ -pe gpu-titanx 2
 #$ -l h_vmem=30G
 
-bash train_explore.sh $SGE_TASK_ID
+bash training_explore.sh $SGE_TASK_ID
