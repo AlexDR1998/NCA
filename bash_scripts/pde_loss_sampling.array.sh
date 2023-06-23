0ba3a1540d6141b9ca511beabdf16293380d6666
Alex <alexander.richardson@gmail.com>
2023-06-23 14:21:05 +0100

    rerunning PDE training parameter sweeps for statistical robustness, ReLU and on finer time sampling

---
+++ bash_scripts/pde_loss_sampling.array.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+#$ -N pde_loss_sampling
+#$ -P scs_schumacher-group 
+#$ -M s1605376@ed.ac.uk
+#$ -cwd
+#$ -l h_rt=47:00:00
+
+#$ -pe gpu-titanx 1
+#$ -l h_vmem=64G
+
+bash pde_loss_sampling.sh $SGE_TASK_ID
---
+++ bash_scripts/pde_loss_sampling.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+#$ -N pde_loss_sampling
+#$ -P scs_schumacher-group 
+#$ -M s1605376@ed.ac.uk
+#$ -cwd
+#$ -l h_rt=47:00:00
+
+#$ -pe gpu-titanx 1
+#$ -l h_vmem=64G
+
+
+. /etc/profile.d/modules.sh
+
+module load anaconda
+source activate nca_tensorflow
+
+python ./pde_loss_sampling.py $1
+source deactivate
