454c911da6a03d4e76fd45f8bb20c1ef8d06918c
Alex <alexander.richardson@gmail.com>
2023-06-28 11:21:10 +0100

    changed GPU type requested

--- bash_scripts/pde_loss_sampling.array.sh
+++ bash_scripts/pde_loss_sampling.array.sh
@@ -3,9 +3,9 @@
 #$ -P scs_schumacher-group 
 #$ -M s1605376@ed.ac.uk
 #$ -cwd
-#$ -l h_rt=24:00:00
+#$ -l h_rt=47:00:00
 
-#$ -pe gpu-titanx 1
+#$ -pe gpu-a100 1
 #$ -l h_vmem=64G
 
 bash pde_loss_sampling.sh $SGE_TASK_ID
--- bash_scripts/pde_loss_sampling.sh
+++ bash_scripts/pde_loss_sampling.sh
@@ -3,9 +3,9 @@
 #$ -P scs_schumacher-group 
 #$ -M s1605376@ed.ac.uk
 #$ -cwd
-#$ -l h_rt=24:00:00
+#$ -l h_rt=47:00:00
 
-#$ -pe gpu-titanx 1
+#$ -pe pe gpu-a100 1
 #$ -l h_vmem=64G
 
 
