19ca9af080870d9b548cc6b311b149daf99e84cd
Alex <alexander.richardson@gmail.com>
2023-12-08 11:34:01 +0000

    fixed stupid bug

--- PDE/trainer/tensorboard_log.py
+++ PDE/trainer/tensorboard_log.py
@@ -33,11 +33,11 @@ class PDE_Train_log(object):
 					#tf.summary.image('True sequence RGB',np.einsum("ncxy->nxyc",data[0,:,:3,...]),step=0,max_outputs=data.shape[0])
 					for t in range(data[b].shape[0]):
 						#tf.summary.image("Final PDE trajectory, batch "+str(b),np.einsum("ncxy->nxyc",trs[b][i][np.newaxis,:3,...]),step=i)
-						tf.summary.image('True sequence RGB',np.einsum("ncxy->nxyc",data[b][t:t+1,:3,...]))
+						tf.summary.image('True sequence RGB',np.einsum("ncxy->nxyc",data[b][t:t+1,:3,...]),step=0)
 				elif self.RGB_mode=="RGBA":
 					#tf.summary.image('True sequence RGBA',np.einsum("ncxy->nxyc",data[0,:,:4,...]),step=0,max_outputs=data.shape[0])
 					for t in range(data[b].shape[0]):
-						tf.summary.image('True sequence RGBA',np.einsum("ncxy->nxyc",data[b][t:t+1,:4,...]))
+						tf.summary.image('True sequence RGBA',np.einsum("ncxy->nxyc",data[b][t:t+1,:4,...]).step=0)
 			
 		self.train_summary_writer = train_summary_writer
 
